91f129f0d1f3fea9f634920b09fa9342ebc1ebe4
Nikolai Trusov <nickolaytrusov01@gmail.com>
2025-09-13 17:14:33 +0300

    feat: add logger into use case

--- src/app/application/use_cases/lead_changed.py
+++ src/app/application/use_cases/lead_changed.py
@@ -15,8 +15,24 @@ class LeadChangedUseCase:
     sheets_gateway: SheetsGateway
 
     def __call__(self, lead_id: int) -> None:
+        logger.info("Checking changes for lead_id=%s", lead_id)
+
         lead = self.lead_gateway.get(lead_id)
+        if lead is None:
+            logger.warning("Lead with lead_id=%s not found", lead_id)
+            return
+
         row_id = self.sheets_lead_gateway.get_row_by_lead(lead_id)
         if not row_id:
+            logger.info("No row found in Sheets for lead_id=%s", lead_id)
             return
+
+        logger.info(
+            "Updating row=%s in Sheets with email=%s, amount=%s for lead_id=%s",
+            row_id,
+            lead.email,
+            lead.amount,
+            lead_id,
+        )
         self.sheets_gateway.update_lead(row_id, lead.email, lead.amount)
+        logger.info("Row=%s successfully updated for lead_id=%s", row_id, lead_id)
